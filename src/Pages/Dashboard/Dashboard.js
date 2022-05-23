c27ff64f215ac2ef116bf3e25072c366356203b4
bmsani <bmsani6@gmail.com>
2022-05-24 03:49:53 +0600

    admin validation created

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,32 +1,42 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
-        <h1 className='text-5xl text-primary'>Dashboard</h1>
-    <Outlet></Outlet>
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-  
-  </div> 
-  <div class="drawer-side">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-      <li><Link to='/dashboard'> My Orders </Link></li>
-      <li><Link to='/dashboard/addReview'> Add Review </Link></li>
-      <li><Link to='/dashboard/myProfile'> My Profile </Link></li>
-      <li><Link to='/dashboard/manageOrder'> Manage All Orders </Link></li>
-      <li><Link to='/dashboard/addProduct'> Add a Product </Link></li>
-      <li><Link to='/dashboard/manageUser'> Manage Users </Link></li>
-      <li><Link to='/dashboard/manageProducts'> Manage Products </Link></li>
-      
-      
-    </ul>
-  
-  </div>
-</div>
+            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content flex flex-col">
+                <h1 className='text-5xl text-primary'>Dashboard</h1>
+                <Outlet></Outlet>
+                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+            </div>
+            <div class="drawer-side">
+                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                    <li><Link to='/dashboard'> My Orders </Link></li>
+                    <li><Link to='/dashboard/addReview'> Add Review </Link></li>
+                    <li><Link to='/dashboard/myProfile'> My Profile </Link></li>
+                    
+                    {
+                        admin && <>
+                            <li><Link to='/dashboard/manageOrder'> Manage All Orders </Link></li>
+                            <li><Link to='/dashboard/addProduct'> Add a Product </Link></li>
+                            <li><Link to='/dashboard/manageUser'> Manage Users </Link></li>
+                            <li><Link to='/dashboard/manageProducts'> Manage Products </Link></li>
+                        </>
+                    }
+
+
+                </ul>
+
+            </div>
+        </div>
     );
 };
 
--- src/Pages/Dashboard/ManageUsers.js
+++ src/Pages/Dashboard/ManageUsers.js
@@ -1,15 +1,66 @@
+import cogoToast from 'cogo-toast';
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
 
 const ManageUsers = () => {
-    const {data:users, isLoading, refetch} = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()))
-    if(isLoading){
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'Get',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+    if (isLoading) {
         return <Loading></Loading>
     }
+    const handleAdmin = email => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    cogoToast.error('Failed to make admin');
+                }
+                return res.json()
+            })
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    cogoToast.success('Successfully made an admin');
+                }
+            })
+    }
     return (
-        <div>
-            <h1 className="text-9xl">Manage users {users.length}</h1>
+        <div class="overflow-x-auto">
+            <table class="table w-full">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index) => <tr className={`${index % 2 === 0 && 'active'}`}>
+                            <th>{index + 1}</th>
+                            <td>{user.email}</td>
+                            {
+                                user.role !== 'admin'
+                                &&
+                                <td>
+                                    <button onClick={() => handleAdmin(user.email)} className='btn btn-xs'> Make Admin </button>
+                                </td>
+                            }
+                        </tr>)
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
